d88f90675679333299d46395a2cba503a0186b92
honghue.tranthi <01694993575>
2018-10-31 17:46:00 +0700

    method overriding

---
+++ b14-method-overriding/index.js
@@ -0,0 +1,16 @@
+class CoffeeMachine {
+    makeCoffee() {
+        console.log('making coffee!')
+    }
+}
+
+class CoffeeMachineSpecial extends CoffeeMachine {
+    makeCoffee() {
+        console.log('made coffee!')
+    }
+}
+
+const coffeeMachineSpecial = new CoffeeMachineSpecial();
+coffeeMachineSpecial.makeCoffee(function () {
+    console.log('ok!')
+});
