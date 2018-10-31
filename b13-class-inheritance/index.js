87a7f19445458c039c76cd8d7b0bdbb96cd238bd
honghue.tranthi <01694993575>
2018-10-31 17:31:42 +0700

    class inheritance

---
+++ b13-class-inheritance/index.js
@@ -0,0 +1,19 @@
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    eat() {
+        console.log('Eating...');
+    }
+}
+
+class Bird  extends Animal {
+    fly() {
+        console.log('Flying');
+    }
+}
+
+const bird = new Bird('vet');
+bird.eat();
+bird.fly();
