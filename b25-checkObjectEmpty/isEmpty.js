1659cf62b512952b41501f1f6f8033d4961b8607
hue <huetth@davido.vn>
2019-01-20 00:27:17 +0700

    add

---
+++ b25-checkObjectEmpty/isEmpty.js
@@ -0,0 +1,18 @@
+let person = {
+    name : 'Hue',
+    point: 10
+};
+
+let person2 = {} ;
+//returns 0 if empty or an integer > 0 if non-empty
+function isEmpty1( obj ) {
+    return Object.keys(obj).length === 0;
+}
+
+isEmpty1( person );
+
+function isEmpty2(obj) {
+    for(var i in obj) { return false; }
+    return true;
+}
+isEmpty2( person2 );
